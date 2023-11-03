012202c22f4f229a4ffc0ae493f3dfc8707d59fe
jakubserwin <kuba.serwin99@gmail.com>
2023-11-03 15:33:04 +0100

    feat: prisma client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
+import { prismaClient } from '.'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
   providers: [],
 }
