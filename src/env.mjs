4131e16209ae159c7b9c579e5a8367ef07d76bac
jakubserwin <kuba.serwin99@gmail.com>
2023-11-07 16:12:31 +0100

    feat: setup t3 env

--- src/env.mjs
+++ src/env.mjs
@@ -3,14 +3,8 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
-    OPEN_AI_API_KEY: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
-  },
-  experimental__runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
 })
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,15 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { env } from '@/env.mjs'
 import { prismaClient } from '.'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prismaClient),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 }
