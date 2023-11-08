b94d9cce89249579d335af8c526c0f892040aa30
jakubserwin <kuba.serwin99@gmail.com>
2023-11-08 15:15:56 +0100

    feat: setup shadcn/ui

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,52 @@
+'use client'
+
+// TODO
+// 0. setup shadcn/ui
+// 1. Create UI layout with card in the middle about signup with google
+// 2. after successfull signup redirect to dashboard page
+// 3. check nextauth session / route guard / middleware (?)
+// 4. dashboard layout UI
+
+import { FormEvent } from 'react'
+import { signIn } from 'next-auth/react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export default function SignUp() {
-  return <div>hello world</div>
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    signIn('google', {
+      callbackUrl: '/dashboard',
+    })
+  }
+
+  // return (
+  //   <form onSubmit={e => handleSubmit(e)}>
+  //     <button type='submit'>Continue with Google</button>
+  //   </form>
+  // )
+
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <Card className='w-full max-w-[420px]'>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
