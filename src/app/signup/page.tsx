cd733b911aebd25ed5fa176d2864c3d1338a7756
jakubserwin <kuba.serwin99@gmail.com>
2023-11-08 15:28:56 +0100

    fix: revert changes to working app

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+// TODO
+// 0. setup shadcn/ui
+// 1. Create UI layout with card in the middle about signup with google
+// 2. after successfull signup redirect to dashboard page
+// 3. check nextauth session / route guard / middleware (?)
+// 4. dashboard layout UI
+
+import { FormEvent } from 'react'
+import { signIn } from 'next-auth/react'
+
 export default function SignUp() {
-  return <div>hello world</div>
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    signIn('google', {
+      callbackUrl: '/dashboard',
+    })
+  }
+
+  // return (
+  //   <form onSubmit={e => handleSubmit(e)}>
+  //     <button type='submit'>Continue with Google</button>
+  //   </form>
+  // )
+
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <button onClick={handleSubmit}>test this button</button>
+    </div>
+  )
 }
